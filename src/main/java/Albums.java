d22a4b87e5b3383bea477defae78336fade7b94d
AlexRios <riosalexandra91@gmail.com>
2022-07-20 11:15:51 -0500

    added files for albums

---
+++ src/main/java/Albums.java
@@ -0,0 +1,72 @@
+import java.io.Serializable;
+import java.util.Date;
+
+public class Albums implements Serializable {
+
+    private long id;
+    private String artist;
+    private String name;
+    private Date release_date;
+    private double sales;
+    private String genre;
+
+    public Albums() {
+    }
+
+    public Albums(long id, String artist, String name, Date release_date, double sales, String genre) {
+        this.id = id;
+        this.artist = artist;
+        this.name = name;
+        this.release_date = release_date;
+        this.sales = sales;
+        this.genre = genre;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getArtist() {
+        return artist;
+    }
+
+    public void setArtist(String artist) {
+        this.artist = artist;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getRelease_date() {
+        return release_date;
+    }
+
+    public void setRelease_date(Date release_date) {
+        this.release_date = release_date;
+    }
+
+    public double getSales() {
+        return sales;
+    }
+
+    public void setSales(double sales) {
+        this.sales = sales;
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre) {
+        this.genre = genre;
+    }
+}
