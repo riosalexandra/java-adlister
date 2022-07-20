47edfec03425976d16a71c8ee62881f417063bd7
AlexRios <riosalexandra91@gmail.com>
2022-07-20 11:12:09 -0500

    added files for view color servlet

---
+++ src/main/java/ViewColorServlet.java
@@ -0,0 +1,19 @@
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet("/viewcolor")
+public class ViewColorServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String color = request.getParameter("color");
+        request.setAttribute("color", color);
+        request.getRequestDispatcher("/View-color.jsp").forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
